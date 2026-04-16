449d54c43dfb7e1ae2b85d32bfbbd0241c445724
0xArCHDeViL <mczerodevil@gmail.com>
2026-04-16 13:38:47 +0700

    chore: downgrade optimization level from -O3 to -O2 in KCFLAGS

--- sweet.sh
+++ sweet.sh
@@ -191,7 +191,7 @@ setup_environment() {
             CROSS_COMPILE=aarch64-linux-android-
             CROSS_COMPILE_ARM32=arm-linux-gnueabi-
             CLANG_TRIPLE=aarch64-linux-gnu-
-            KCFLAGS="-O3 -mllvm -inline-threshold=200 -mllvm -polly -mllvm -polly-ast-use-context -mllvm -polly-vectorizer=stripmine -Wno-declaration-after-statement -Wno-unused-variable -Wno-void-pointer-to-int-cast"
+            KCFLAGS="-O2 -mllvm -inline-threshold=200 -mllvm -polly -mllvm -polly-ast-use-context -mllvm -polly-vectorizer=stripmine -Wno-declaration-after-statement -Wno-unused-variable -Wno-void-pointer-to-int-cast"
         )
     elif [[ "$TOOLCHAIN_SELECTOR" == "lilium" ]]; then
         export MAKE_ARGS=(
